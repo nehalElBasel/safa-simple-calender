0499f4953eb739bbbecc4f5637f1d2c3d0536ae2
nehal1992 <nehal.khaled@olarm.com>
2021-02-18 14:59:16 +0200

    remove unused code

--- app/Http/Controllers/CalenderTaskController.php
+++ app/Http/Controllers/CalenderTaskController.php
@@ -65,7 +65,7 @@ class CalenderTaskController extends Controller
          */
 
         $k = 0;
-        //$first_session = false;
+
         //if start date day exists in days list work so clender start with it for first session $i=0
         if (in_array($start_date_day, $days_list)) {
             $calender[0] = $start_date->format('Y-m-d');
@@ -82,7 +82,6 @@ class CalenderTaskController extends Controller
                 //ignore first session days if less than startdate
                 //if start date day exists in days list work so clender start with it for first session $i=0
                 if (($days_list[$j] < $start_date_day && $i == 0) || $days_list[$j] == $start_date_day && $i == 0) {
-                    //echo $start_date->format('Y-m-d');
                     continue;
                 }
 
@@ -95,7 +94,7 @@ class CalenderTaskController extends Controller
         return response()->json([
             'success'  => true,
             'sessions' => $calender,
-            'count'    => count($calender),
+            // 'count'    => count($calender),
         ], 200);
 
     }
